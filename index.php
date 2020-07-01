184b4d99c22aae07bdaee26637be57aecaa53fe5
ʀɪᴛᴇꜱʜ ᴋᴜꜱʜᴡᴀʜᴀ <67673777+aiedrow@users.noreply.github.com>
2020-07-01 11:08:58 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php include("includes/header.html"); ?>
+
+<div id='content'>
+<center><b style='font-size:121%;'>⚡ Explore</b></center> <br>
+
+
+<center>
+    
+<button class="content" style="background:url(https://media.giphy.com/media/l3V0hSwrWKBGIM2vS/giphy.gif);
+overflow: hidden;">
+<j style="background:#333;padding: 1000px;position: static;margin-left: -24px;opacity: 37%;z-index: 1;"></j>
+<b style="position: relative;">Welcome to Cube</b><br> 
+<small style="position: relative;"><small><j>7 collaborators</j></small></small></button>
+    
+<button class="content" style="background:url(https://media.giphy.com/media/l3V0hSwrWKBGIM2vS/giphy.gif);
+overflow: hidden;">
+<j style="background:#333;padding: 1000px;position: static;margin-left: -24px;opacity: 37%;z-index: 1;"></j>
+<b style="position: relative;">Welcome to Cube</b><br> 
+<small style="position: relative;"><small><j>7 collaborators</j></small></small></button>
+
+</center>
+
+
+
+
+
+
+</div>
+</body>
+<style>*{outline:none;}</style>
+</html>
